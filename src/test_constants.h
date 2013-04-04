1ba9c45906777e01edcdfb3d56638ce2e3e73cb3
Sahand Kashani-Akhavan <sahand.kashani-akhavan@epfl.ch>
2013-04-04 12:42:29 +0200

    max NUMBER OF TESTS which doesn't crash server

--- src/test_constants.h
+++ src/test_constants.h
@@ -3,9 +3,6 @@
 
 // #define NUMBER_OF_TESTS ((unsigned long int) 65536)    // 2^16
 // #define NUMBER_OF_TESTS ((unsigned long int) 262144)   // 2^18
-// #define NUMBER_OF_TESTS ((unsigned long int) 1048576)  // 2^20
-// #define NUMBER_OF_TESTS ((unsigned long int) 4194304)  // 2^22
-#define NUMBER_OF_TESTS ((unsigned long int) 16777216) // 2^24
-// #define NUMBER_OF_TESTS ((unsigned long int) 67108864) // 2^26
+#define NUMBER_OF_TESTS ((unsigned long int) 1048576)  // 2^20
 
 #endif
