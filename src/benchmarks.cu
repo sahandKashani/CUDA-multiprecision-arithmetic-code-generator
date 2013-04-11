56779d2b51e496afd9cd780d46523fdabd2f3331
Sahand Kashani-Akhavan <sahand.kashani-akhavan@epfl.ch>
2013-04-11 14:06:47 +0200

    updated for reading bignums from files, because it takes time to generate them each time

--- src/benchmarks.cu
+++ src/benchmarks.cu
@@ -7,50 +7,89 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-void generate_operands(uint32_t* host_a, uint32_t* host_b);
+void generate_random_bignum_arrays_to_files();
+void generate_random_bignum_array_to_file(const char* file_name);
+void read_bignum_array_from_file(const char* file_name, uint32_t* bignum, uint32_t amount_to_read);
+void write_bignum_array_to_file(const char* file_name, uint32_t* bignum);
 
 int main(void)
 {
     // host operands (host_a, host_b) and results (host_c)
-    uint32_t* host_a = (uint32_t*) calloc(NUMBER_OF_BIGNUMS * BIGNUM_NUMBER_OF_WORDS, sizeof(uint32_t));
-    uint32_t* host_b = (uint32_t*) calloc(NUMBER_OF_BIGNUMS * BIGNUM_NUMBER_OF_WORDS, sizeof(uint32_t));
-    uint32_t* host_c = (uint32_t*) calloc(NUMBER_OF_BIGNUMS * BIGNUM_NUMBER_OF_WORDS, sizeof(uint32_t));
+    // uint32_t* host_a = (uint32_t*) calloc(NUMBER_OF_BIGNUMS * BIGNUM_NUMBER_OF_WORDS, sizeof(uint32_t));
+    // uint32_t* host_b = (uint32_t*) calloc(NUMBER_OF_BIGNUMS * BIGNUM_NUMBER_OF_WORDS, sizeof(uint32_t));
+    // uint32_t* host_c = (uint32_t*) calloc(NUMBER_OF_BIGNUMS * BIGNUM_NUMBER_OF_WORDS, sizeof(uint32_t));
 
-    // generate random numbers for the tests
-    generate_operands(host_a, host_b);
+    generate_random_bignum_arrays_to_files();
 
-    coalesced_normal_memory_layout_benchmark(host_c, host_a, host_b, BLOCKS_PER_GRID, THREADS_PER_BLOCK);
-    addition_check(host_c, host_a, host_b);
+    // coalesced_normal_memory_layout_benchmark(host_c, host_a, host_b, BLOCKS_PER_GRID, THREADS_PER_BLOCK);
+    // addition_check(host_c, host_a, host_b);
 
-    free(host_a);
-    free(host_b);
-    free(host_c);
+    // free(host_a);
+    // free(host_b);
+    // free(host_c);
 
     // for leak detection when using cuda-memcheck
     cudaDeviceReset();
 }
 
-/**
- * Generates random numbers and assigns them to the 2 bignum arrays passed as a
- * parameter.
- * @param host_a first array to populate
- * @param host_b second array to populate
- */
-void generate_operands(uint32_t* host_a, uint32_t* host_b)
+void generate_random_bignum_arrays_to_files()
 {
-    printf("generating operands ... ");
-    fflush(stdout);
-
     start_random_number_generator();
+
+    generate_random_bignum_array_to_file("../data/bignum_array_1.txt");
+    generate_random_bignum_array_to_file("../data/bignum_array_2.txt");
+
+    stop_random_number_generator();
+}
+
+void generate_random_bignum_array_to_file(const char* file_name)
+{
+    uint32_t* bignum = (uint32_t*) calloc(BIGNUM_NUMBER_OF_WORDS, sizeof(uint32_t));
+    FILE* file = fopen(file_name, "w");
 
     for (uint32_t i = 0; i < NUMBER_OF_BIGNUMS; i++)
     {
-        generate_random_bignum(&host_a[IDX(i, 0)]);
-        generate_random_bignum(&host_b[IDX(i, 0)]);
+        generate_random_bignum(bignum);
+
+        for (uint32_t j = 0; j < BIGNUM_NUMBER_OF_WORDS; j++)
+        {
+            fprintf(file, "%u ", bignum[j]);
+        }
+
+        fprintf(file, "\n");
+    }
+}
+
+void read_bignum_array_from_file(const char* file_name, uint32_t* bignum, uint32_t amount_to_read)
+{
+    FILE* file = fopen(file_name, "r");
+
+    for (uint32_t i = 0; i < amount_to_read; i++)
+    {
+        for (uint32_t j = 0; j < BIGNUM_NUMBER_OF_WORDS; j++)
+        {
+            fscanf(file, "%u", &bignum[IDX(i, j)]);
+        }
+
+        fprintf(file, "\n");
     }
 
-    stop_random_number_generator();
+    fclose(file);
+}
 
-    printf("done\n");
-    fflush(stdout);
+void write_bignum_array_to_file(const char* file_name, uint32_t* bignum)
+{
+    FILE* file = fopen(file_name, "w");
+
+    for (uint32_t i = 0; i < NUMBER_OF_BIGNUMS; i++)
+    {
+        for (uint32_t j = 0; j < BIGNUM_NUMBER_OF_WORDS; j++)
+        {
+            fprintf(file, "%u ", bignum[IDX(i, j)]);
+        }
+
+        fprintf(file, "\n");
+    }
+
+    fclose(file);
 }
