56ca17b079202702e979d1b9cf9728bf73ab1235
Sahand Kashani-Akhavan <sahand.kashani-akhavan@epfl.ch>
2013-05-03 10:19:00 +0200

    addition and subtraction testing fully work. Back to getting f!#ing multiplication to work

--- scripts/operation_checker.py
+++ scripts/operation_checker.py
@@ -11,7 +11,7 @@ from constants import add_results_file_name
 from constants import sub_results_file_name
 from constants import mul_results_file_name
 
-# # operands
+# operands
 a = read_numbers_from_file_coalesced(coalesced_a_file_name)
 b = read_numbers_from_file_coalesced(coalesced_b_file_name)
 
@@ -23,37 +23,42 @@ for (op1, op2, result) in zip(a, b, add_results):
     expected_result = op1 + op2
     if expected_result != result:
         print("\nadd error:")
-        print(str(op1))
-        print(str(op2))
-        print(str(expected_result))
+        print("op1             = " + str(op1))
+        print("op2             = " + str(op2))
+        print("result          = " + str(result))
+        print("expected_result = " + str(expected_result))
         break
+
 print("done")
 
-# # check subtraction results
-# sub_results = read_numbers_from_file_coalesced(sub_results_file_name)
-# for i in sub_results:
-#     print(str(i))
+# check subtraction results
+print("checking sub results => ", end='')
+sub_results = read_numbers_from_file_coalesced(sub_results_file_name)
 
-# for (op1, op2, result) in zip(a, b, sub_results):
-#     expected_result = op1 - op2
-#     if expected_result != result:
-#         print("\nsub error:")
-#         print(str(op1))
-#         print(str(op2))
-#         print(str(result))
-#         print(str(expected_result))
-#         # print(str(expected_result % (1 << precision)) + "=")
-#         break
-# print("done")
+for (op1, op2, result) in zip(a, b, sub_results):
+    expected_result = op1 - op2
+    if expected_result != result:
+        print("\nsub error:")
+        print("op1             = " + str(op1))
+        print("op2             = " + str(op2))
+        print("result          = " + str(result))
+        print("expected_result = " + str(expected_result))
+        break
 
-# # check multiplication results
-# mul_results = read_numbers_from_file_coalesced(mul_results_file_name)
-# for (op1, op2, result) in zip(a, b, mul_results):
-#     expected_result = op1 * op2
-#     if expected_result != result:
-#         print("\nmul error:")
-#         print(to_binary_string(op1))
-#         print(to_binary_string(op2))
-#         print(to_binary_string(expected_result))
-#         break
-# print("done")
+print("done")
+
+# check subtraction results
+print("checking mul results => ", end='')
+mul_results = read_numbers_from_file_coalesced(mul_results_file_name)
+
+for (op1, op2, result) in zip(a, b, mul_results):
+    expected_result = op1 * op2
+    if expected_result != result:
+        print("\nmul error:")
+        print("op1             = " + str(op1))
+        print("op2             = " + str(op2))
+        print("result          = " + str(result))
+        print("expected_result = " + str(expected_result))
+        break
+
+print("done")
