b32090a2f89b0341f33b987620668b69af0ae673
Sahand Kashani-Akhavan <sahand.kashani-akhavan@epfl.ch>
2013-05-12 21:14:42 +0200

    Added error checking code for modular addition. There is currently an error on the last 2 hexadecimal digits of the output of the modular addition, but all the rest of the number is perfectly identical.

--- scripts/operation_checker.py
+++ scripts/operation_checker.py
@@ -2,9 +2,11 @@ from input_output import read_numbers_from_file_coalesced
 
 from constants import coalesced_a_file_name
 from constants import coalesced_b_file_name
+from constants import coalesced_m_file_name
 from constants import add_results_file_name
 from constants import sub_results_file_name
 from constants import mul_results_file_name
+from constants import add_m_results_file_name
 
 from conversions import int_to_hex_str
 
@@ -28,12 +30,35 @@ def binary_operator_check(result_file_name, op1, op2, op_name, py_op_symbol, res
     if not error:
         print("ok")
 
+def modular_binary_operator_check(result_file_name, op1, op2, op3, op_name, py_op_symbol):
+    res = read_numbers_from_file_coalesced(result_file_name)
+    print("Checking \"" + op_name + "\" results => ", end = '')
+
+    error = False
+    for (i, a, b, m, c) in zip(range(len(res)), op1, op2, op3, res):
+        expected = eval('a' + py_op_symbol + 'b' + '% m')
+        if c != expected:
+            error = True
+            print("error")
+            print("\n" + op_name + " error from thread " + str(i) + ":")
+            print("a        = " + int_to_hex_str(a, False))
+            print("b        = " + int_to_hex_str(b, False))
+            print("m        = " + int_to_hex_str(m, False))
+            print("c        = " + int_to_hex_str(c, False))
+            print("expected = " + int_to_hex_str(expected, False))
+            break
+
+    if not error:
+        print("ok")
+
 print("Checking operation results:")
 
 # operands
 a = read_numbers_from_file_coalesced(coalesced_a_file_name)
 b = read_numbers_from_file_coalesced(coalesced_b_file_name)
+m = read_numbers_from_file_coalesced(coalesced_m_file_name)
 
-binary_operator_check(add_results_file_name, a, b, "add", "+", False)
-binary_operator_check(sub_results_file_name, a, b, "sub", "-", False)
-binary_operator_check(mul_results_file_name, a, b, "mul", "*", True)
+# binary_operator_check(add_results_file_name, a, b, "add", "+", False)
+# binary_operator_check(sub_results_file_name, a, b, "sub", "-", False)
+# binary_operator_check(mul_results_file_name, a, b, "mul", "*", True)
+modular_binary_operator_check(add_m_results_file_name, a, b, m, "add_m", "+")
--- src/benchmarks.cu
+++ src/benchmarks.cu
@@ -15,16 +15,16 @@ void sub_benchmark();
 void mul_benchmark();
 void add_m_benchmark();
 
+// local data kernels
 __global__ void add_loc_kernel(uint32_t* dev_c, uint32_t* dev_a, uint32_t* dev_b);
-__global__ void add_glo_kernel(uint32_t* dev_c, uint32_t* dev_a, uint32_t* dev_b);
-
 __global__ void sub_loc_kernel(uint32_t* dev_c, uint32_t* dev_a, uint32_t* dev_b);
-__global__ void sub_glo_kernel(uint32_t* dev_c, uint32_t* dev_a, uint32_t* dev_b);
-
 __global__ void mul_loc_kernel(uint32_t* dev_c, uint32_t* dev_a, uint32_t* dev_b);
-__global__ void mul_glo_kernel(uint32_t* dev_c, uint32_t* dev_a, uint32_t* dev_b);
-
 __global__ void add_m_loc_kernel(uint32_t* dev_c, uint32_t* dev_a, uint32_t* dev_b, uint32_t* dev_m);
+
+// global data kernels
+__global__ void add_glo_kernel(uint32_t* dev_c, uint32_t* dev_a, uint32_t* dev_b);
+__global__ void sub_glo_kernel(uint32_t* dev_c, uint32_t* dev_a, uint32_t* dev_b);
+__global__ void mul_glo_kernel(uint32_t* dev_c, uint32_t* dev_a, uint32_t* dev_b);
 
 ////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////// BENCHMARKS /////////////////////////////////
@@ -32,71 +32,71 @@ __global__ void add_m_loc_kernel(uint32_t* dev_c, uint32_t* dev_a, uint32_t* dev
 
 void add_benchmark()
 {
-    // uint32_t* host_a = (uint32_t*) calloc(NUMBER_OF_BIGNUMS * MIN_BIGNUM_NUMBER_OF_WORDS, sizeof(uint32_t));
-    // uint32_t* host_b = (uint32_t*) calloc(NUMBER_OF_BIGNUMS * MIN_BIGNUM_NUMBER_OF_WORDS, sizeof(uint32_t));
-    // uint32_t* host_c = (uint32_t*) calloc(NUMBER_OF_BIGNUMS * MIN_BIGNUM_NUMBER_OF_WORDS, sizeof(uint32_t));
+    uint32_t* host_a = (uint32_t*) calloc(NUMBER_OF_BIGNUMS * MIN_BIGNUM_NUMBER_OF_WORDS, sizeof(uint32_t));
+    uint32_t* host_b = (uint32_t*) calloc(NUMBER_OF_BIGNUMS * MIN_BIGNUM_NUMBER_OF_WORDS, sizeof(uint32_t));
+    uint32_t* host_c = (uint32_t*) calloc(NUMBER_OF_BIGNUMS * MIN_BIGNUM_NUMBER_OF_WORDS, sizeof(uint32_t));
 
-    // assert(host_a != NULL);
-    // assert(host_b != NULL);
-    // assert(host_c != NULL);
+    assert(host_a != NULL);
+    assert(host_b != NULL);
+    assert(host_c != NULL);
 
-    // read_coalesced_bignums_from_file(COALESCED_A_FILE_NAME, host_a, false);
-    // read_coalesced_bignums_from_file(COALESCED_B_FILE_NAME, host_b, false);
+    read_coalesced_bignums_from_file(COALESCED_A_FILE_NAME, host_a, false);
+    read_coalesced_bignums_from_file(COALESCED_B_FILE_NAME, host_b, false);
 
-    // binary_operator_benchmark(host_c, host_a, host_b, add_loc_kernel, "add_loc", false);
-    // binary_operator_benchmark(host_c, host_a, host_b, add_glo_kernel, "add_glo", false);
+    binary_operator_benchmark(host_c, host_a, host_b, add_loc_kernel, "add_loc", false);
+    binary_operator_benchmark(host_c, host_a, host_b, add_glo_kernel, "add_glo", false);
 
-    // write_coalesced_bignums_to_file(ADD_RESULTS_FILE_NAME, host_c, false);
+    write_coalesced_bignums_to_file(ADD_RESULTS_FILE_NAME, host_c, false);
 
-    // free(host_a);
-    // free(host_b);
-    // free(host_c);
+    free(host_a);
+    free(host_b);
+    free(host_c);
 }
 
 void sub_benchmark()
 {
-    // uint32_t* host_a = (uint32_t*) calloc(NUMBER_OF_BIGNUMS * MIN_BIGNUM_NUMBER_OF_WORDS, sizeof(uint32_t));
-    // uint32_t* host_b = (uint32_t*) calloc(NUMBER_OF_BIGNUMS * MIN_BIGNUM_NUMBER_OF_WORDS, sizeof(uint32_t));
-    // uint32_t* host_c = (uint32_t*) calloc(NUMBER_OF_BIGNUMS * MIN_BIGNUM_NUMBER_OF_WORDS, sizeof(uint32_t));
+    uint32_t* host_a = (uint32_t*) calloc(NUMBER_OF_BIGNUMS * MIN_BIGNUM_NUMBER_OF_WORDS, sizeof(uint32_t));
+    uint32_t* host_b = (uint32_t*) calloc(NUMBER_OF_BIGNUMS * MIN_BIGNUM_NUMBER_OF_WORDS, sizeof(uint32_t));
+    uint32_t* host_c = (uint32_t*) calloc(NUMBER_OF_BIGNUMS * MIN_BIGNUM_NUMBER_OF_WORDS, sizeof(uint32_t));
 
-    // assert(host_a != NULL);
-    // assert(host_b != NULL);
-    // assert(host_c != NULL);
+    assert(host_a != NULL);
+    assert(host_b != NULL);
+    assert(host_c != NULL);
 
-    // read_coalesced_bignums_from_file(COALESCED_A_FILE_NAME, host_a, false);
-    // read_coalesced_bignums_from_file(COALESCED_B_FILE_NAME, host_b, false);
+    read_coalesced_bignums_from_file(COALESCED_A_FILE_NAME, host_a, false);
+    read_coalesced_bignums_from_file(COALESCED_B_FILE_NAME, host_b, false);
 
-    // binary_operator_benchmark(host_c, host_a, host_b, sub_loc_kernel, "sub_loc", false);
-    // binary_operator_benchmark(host_c, host_a, host_b, sub_glo_kernel, "sub_glo", false);
+    binary_operator_benchmark(host_c, host_a, host_b, sub_loc_kernel, "sub_loc", false);
+    binary_operator_benchmark(host_c, host_a, host_b, sub_glo_kernel, "sub_glo", false);
 
-    // write_coalesced_bignums_to_file(SUB_RESULTS_FILE_NAME, host_c, false);
+    write_coalesced_bignums_to_file(SUB_RESULTS_FILE_NAME, host_c, false);
 
-    // free(host_a);
-    // free(host_b);
-    // free(host_c);
+    free(host_a);
+    free(host_b);
+    free(host_c);
 }
 
 void mul_benchmark()
 {
-    // uint32_t* host_a = (uint32_t*) calloc(NUMBER_OF_BIGNUMS * MIN_BIGNUM_NUMBER_OF_WORDS, sizeof(uint32_t));
-    // uint32_t* host_b = (uint32_t*) calloc(NUMBER_OF_BIGNUMS * MIN_BIGNUM_NUMBER_OF_WORDS, sizeof(uint32_t));
-    // uint32_t* host_c = (uint32_t*) calloc(NUMBER_OF_BIGNUMS * MAX_BIGNUM_NUMBER_OF_WORDS, sizeof(uint32_t));
+    uint32_t* host_a = (uint32_t*) calloc(NUMBER_OF_BIGNUMS * MIN_BIGNUM_NUMBER_OF_WORDS, sizeof(uint32_t));
+    uint32_t* host_b = (uint32_t*) calloc(NUMBER_OF_BIGNUMS * MIN_BIGNUM_NUMBER_OF_WORDS, sizeof(uint32_t));
+    uint32_t* host_c = (uint32_t*) calloc(NUMBER_OF_BIGNUMS * MAX_BIGNUM_NUMBER_OF_WORDS, sizeof(uint32_t));
 
-    // assert(host_a != NULL);
-    // assert(host_b != NULL);
-    // assert(host_c != NULL);
+    assert(host_a != NULL);
+    assert(host_b != NULL);
+    assert(host_c != NULL);
 
-    // read_coalesced_bignums_from_file(COALESCED_A_FILE_NAME, host_a, false);
-    // read_coalesced_bignums_from_file(COALESCED_B_FILE_NAME, host_b, false);
+    read_coalesced_bignums_from_file(COALESCED_A_FILE_NAME, host_a, false);
+    read_coalesced_bignums_from_file(COALESCED_B_FILE_NAME, host_b, false);
 
-    // binary_operator_benchmark(host_c, host_a, host_b, mul_loc_kernel, "mul_loc", true);
-    // binary_operator_benchmark(host_c, host_a, host_b, mul_glo_kernel, "mul_glo", true);
+    binary_operator_benchmark(host_c, host_a, host_b, mul_loc_kernel, "mul_loc", true);
+    binary_operator_benchmark(host_c, host_a, host_b, mul_glo_kernel, "mul_glo", true);
 
-    // write_coalesced_bignums_to_file(MUL_RESULTS_FILE_NAME, host_c, true);
+    write_coalesced_bignums_to_file(MUL_RESULTS_FILE_NAME, host_c, true);
 
-    // free(host_a);
-    // free(host_b);
-    // free(host_c);
+    free(host_a);
+    free(host_b);
+    free(host_c);
 }
 
 void add_m_benchmark()
@@ -122,6 +122,7 @@ void add_m_benchmark()
     free(host_a);
     free(host_b);
     free(host_c);
+    free(host_m);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -153,7 +154,6 @@ __global__ void add_loc_kernel(uint32_t* dev_c, uint32_t* dev_a, uint32_t* dev_b
     // uint32_t b[MIN_BIGNUM_NUMBER_OF_WORDS];
     // uint32_t c[MIN_BIGNUM_NUMBER_OF_WORDS];
 
-    // // #pragma unroll
     // for (uint32_t i = 0; i < MIN_BIGNUM_NUMBER_OF_WORDS; i++)
     // {
     //     a[i] = dev_a[COAL_IDX(i, tid)];
@@ -172,7 +172,6 @@ __global__ void add_loc_kernel(uint32_t* dev_c, uint32_t* dev_a, uint32_t* dev_b
     // add_loc(c, a, b);
     // add_loc(c, a, b);
 
-    // // #pragma unroll
     // for (uint32_t i = 0; i < MIN_BIGNUM_NUMBER_OF_WORDS; i++)
     // {
     //     dev_c[COAL_IDX(i, tid)] = c[i];
@@ -204,7 +203,6 @@ __global__ void sub_loc_kernel(uint32_t* dev_c, uint32_t* dev_a, uint32_t* dev_b
     // uint32_t b[MIN_BIGNUM_NUMBER_OF_WORDS];
     // uint32_t c[MIN_BIGNUM_NUMBER_OF_WORDS];
 
-    // // #pragma unroll
     // for (uint32_t i = 0; i < MIN_BIGNUM_NUMBER_OF_WORDS; i++)
     // {
     //     a[i] = dev_a[COAL_IDX(i, tid)];
@@ -223,7 +221,6 @@ __global__ void sub_loc_kernel(uint32_t* dev_c, uint32_t* dev_a, uint32_t* dev_b
     // sub_loc(c, a, b);
     // sub_loc(c, a, b);
 
-    // // #pragma unroll
     // for (uint32_t i = 0; i < MIN_BIGNUM_NUMBER_OF_WORDS; i++)
     // {
     //     dev_c[COAL_IDX(i, tid)] = c[i];
@@ -255,7 +252,6 @@ __global__ void mul_loc_kernel(uint32_t* dev_c, uint32_t* dev_a, uint32_t* dev_b
     // uint32_t b[MIN_BIGNUM_NUMBER_OF_WORDS];
     // uint32_t c[MAX_BIGNUM_NUMBER_OF_WORDS];
 
-    // // #pragma unroll
     // for (uint32_t i = 0; i < MIN_BIGNUM_NUMBER_OF_WORDS; i++)
     // {
     //     a[i] = dev_a[COAL_IDX(i, tid)];
@@ -274,7 +270,6 @@ __global__ void mul_loc_kernel(uint32_t* dev_c, uint32_t* dev_a, uint32_t* dev_b
     // mul_loc(c, a, b);
     // mul_loc(c, a, b);
 
-    // // #pragma unroll
     // for (uint32_t i = 0; i < MAX_BIGNUM_NUMBER_OF_WORDS; i++)
     // {
     //     dev_c[COAL_IDX(i, tid)] = c[i];
@@ -290,7 +285,6 @@ __global__ void add_m_loc_kernel(uint32_t* dev_c, uint32_t* dev_a, uint32_t* dev
     uint32_t c[MIN_BIGNUM_NUMBER_OF_WORDS];
     uint32_t m[MIN_BIGNUM_NUMBER_OF_WORDS];
 
-    // #pragma unroll
     for (uint32_t i = 0; i < MIN_BIGNUM_NUMBER_OF_WORDS; i++)
     {
         a[i] = dev_a[COAL_IDX(i, tid)];
@@ -310,7 +304,6 @@ __global__ void add_m_loc_kernel(uint32_t* dev_c, uint32_t* dev_a, uint32_t* dev
     add_m_loc(c, a, b, m);
     add_m_loc(c, a, b, m);
 
-    // #pragma unroll
     for (uint32_t i = 0; i < MIN_BIGNUM_NUMBER_OF_WORDS; i++)
     {
         dev_c[COAL_IDX(i, tid)] = c[i];
@@ -390,41 +383,49 @@ void modular_binary_operator_benchmark(uint32_t* host_c, uint32_t* host_a, uint3
     assert(host_a != NULL);
     assert(host_b != NULL);
     assert(host_c != NULL);
+    assert(host_m != NULL);
     assert(kernel != NULL);
     assert(operation_name != NULL);
 
-    // device operands (dev_a, dev_b) and results (dev_c)
+    // device operands (dev_a, dev_b, dev_m) and results (dev_c)
     uint32_t* dev_a;
     uint32_t* dev_b;
     uint32_t* dev_c;
+    uint32_t* dev_m;
 
     // allocate gpu memory
     cudaError dev_a_malloc_success = cudaMalloc((void**) &dev_a, NUMBER_OF_BIGNUMS * MIN_BIGNUM_NUMBER_OF_WORDS * sizeof(uint32_t));
     cudaError dev_b_malloc_success = cudaMalloc((void**) &dev_b, NUMBER_OF_BIGNUMS * MIN_BIGNUM_NUMBER_OF_WORDS * sizeof(uint32_t));
     cudaError dev_c_malloc_success = cudaMalloc((void**) &dev_c, NUMBER_OF_BIGNUMS * MIN_BIGNUM_NUMBER_OF_WORDS * sizeof(uint32_t));
+    cudaError dev_m_malloc_success = cudaMalloc((void**) &dev_m, NUMBER_OF_BIGNUMS * MIN_BIGNUM_NUMBER_OF_WORDS * sizeof(uint32_t));
     assert(dev_a_malloc_success == cudaSuccess);
     assert(dev_b_malloc_success == cudaSuccess);
     assert(dev_c_malloc_success == cudaSuccess);
+    assert(dev_m_malloc_success == cudaSuccess);
 
     // make sure gpu memory is clean before our calculations (you never know ...)
     cudaError dev_a_cleanup_memset_success = cudaMemset(dev_a, 0, NUMBER_OF_BIGNUMS * MIN_BIGNUM_NUMBER_OF_WORDS * sizeof(uint32_t));
     cudaError dev_b_cleanup_memset_success = cudaMemset(dev_b, 0, NUMBER_OF_BIGNUMS * MIN_BIGNUM_NUMBER_OF_WORDS * sizeof(uint32_t));
     cudaError dev_c_cleanup_memset_success = cudaMemset(dev_c, 0, NUMBER_OF_BIGNUMS * MIN_BIGNUM_NUMBER_OF_WORDS * sizeof(uint32_t));
+    cudaError dev_m_cleanup_memset_success = cudaMemset(dev_m, 0, NUMBER_OF_BIGNUMS * MIN_BIGNUM_NUMBER_OF_WORDS * sizeof(uint32_t));
     assert(dev_a_cleanup_memset_success == cudaSuccess);
     assert(dev_b_cleanup_memset_success == cudaSuccess);
     assert(dev_c_cleanup_memset_success == cudaSuccess);
+    assert(dev_m_cleanup_memset_success == cudaSuccess);
 
     // copy operands to device memory
     cudaError dev_a_memcpy_succes = cudaMemcpy(dev_a, host_a, NUMBER_OF_BIGNUMS * MIN_BIGNUM_NUMBER_OF_WORDS * sizeof(uint32_t), cudaMemcpyHostToDevice);
     cudaError dev_b_memcpy_succes = cudaMemcpy(dev_b, host_b, NUMBER_OF_BIGNUMS * MIN_BIGNUM_NUMBER_OF_WORDS * sizeof(uint32_t), cudaMemcpyHostToDevice);
+    cudaError dev_m_memcpy_succes = cudaMemcpy(dev_m, host_m, NUMBER_OF_BIGNUMS * MIN_BIGNUM_NUMBER_OF_WORDS * sizeof(uint32_t), cudaMemcpyHostToDevice);
     assert(dev_a_memcpy_succes == cudaSuccess);
     assert(dev_b_memcpy_succes == cudaSuccess);
+    assert(dev_m_memcpy_succes == cudaSuccess);
 
     printf("Benchmarking \"%s\" on GPU ... ", operation_name);
     fflush(stdout);
 
     // execute kernel
-    kernel<<<BLOCKS_PER_GRID, THREADS_PER_BLOCK>>>(dev_c, dev_a, dev_b, NULL);
+    kernel<<<BLOCKS_PER_GRID, THREADS_PER_BLOCK>>>(dev_c, dev_a, dev_b, dev_m);
 
     printf("done\n");
     fflush(stdout);
@@ -437,12 +438,15 @@ void modular_binary_operator_benchmark(uint32_t* host_c, uint32_t* host_a, uint3
     dev_a_cleanup_memset_success = cudaMemset(dev_a, 0, NUMBER_OF_BIGNUMS * MIN_BIGNUM_NUMBER_OF_WORDS * sizeof(uint32_t));
     dev_b_cleanup_memset_success = cudaMemset(dev_b, 0, NUMBER_OF_BIGNUMS * MIN_BIGNUM_NUMBER_OF_WORDS * sizeof(uint32_t));
     dev_c_cleanup_memset_success = cudaMemset(dev_c, 0, NUMBER_OF_BIGNUMS * MIN_BIGNUM_NUMBER_OF_WORDS * sizeof(uint32_t));
+    dev_m_cleanup_memset_success = cudaMemset(dev_m, 0, NUMBER_OF_BIGNUMS * MIN_BIGNUM_NUMBER_OF_WORDS * sizeof(uint32_t));
     assert(dev_a_cleanup_memset_success == cudaSuccess);
     assert(dev_b_cleanup_memset_success == cudaSuccess);
     assert(dev_c_cleanup_memset_success == cudaSuccess);
+    assert(dev_m_cleanup_memset_success == cudaSuccess);
 
     // free device memory
     cudaFree(dev_a);
     cudaFree(dev_b);
     cudaFree(dev_c);
+    cudaFree(dev_m);
 }
