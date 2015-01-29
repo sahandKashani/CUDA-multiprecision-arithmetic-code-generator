44317d5a329af645991034cec1b549321dee7b20
Sahand Kashani-Akhavan <sahand.kashani@gmail.com>
2015-01-29 16:00:26 +0100

    deleted files not needed on repo

--- retrieve_benchmarks.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-scp -r pollardrho@lacalpc26.epfl.ch:/home/pollardrho/Pollard-Rho-CUDA/benchmarks/fermi/* benchmarks/fermi/;
-scp -r pollardrho@lacalgpu2.epfl.ch:/home/pollardrho/Pollard-Rho-CUDA/benchmarks/kepler/* benchmarks/kepler/;
