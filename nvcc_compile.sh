f2b2d2c20c2ed1eeeeaa25f2331239656e3104b8
Sahand Kashani-Akhavan <sahand.kashani@gmail.com>
2015-01-29 16:02:07 +0100

    removed compiling and running scripts

--- nvcc_compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ "$#" -eq 1 ]; then
-    cd "../src/"
-    # nvcc -arch=sm_"$1" --ptxas-options=-v main.cu benchmarks.cu input_output.cpp -o ../bin/benchmarks
-    nvcc -arch=sm_"$1" main.cu benchmarks.cu input_output.cpp -o ../bin/benchmarks
-else
-    echo "Please input architecture <20|30>"
-fi;
