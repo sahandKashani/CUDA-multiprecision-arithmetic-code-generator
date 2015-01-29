a7bf259831eb887151bf0221a1118fec168fdfbd
Sahand Kashani-Akhavan <sahand.kashani@gmail.com>
2015-01-29 16:03:15 +0100

    removed files not needed on git repo

--- input_output.h
+++
@@ -1,9 +0,0 @@
-#ifndef INPUT_OUTPUT_H
-#define INPUT_OUTPUT_H
-
-#include <stdint.h>
-
-void read_coalesced_bignums_from_file(const char* file_name, uint32_t* bignums, uint32_t word_count);
-void write_coalesced_bignums_to_file(const char* file_name, uint32_t* bignums, uint32_t word_count);
-
-#endif
